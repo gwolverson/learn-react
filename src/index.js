13a9cb2b377ddc283d925d2a496e3c90058f9c08
Greg Wolverson <greg.wolverson@hotmail.com>
2019-01-25 16:22:50 +0000

    Taking out react gubbins and stripping back to basic app to start from

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './main/App';
 import * as serviceWorker from './serviceWorker';
+import './main/styles/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
